5c5280960d71f9e84390e1a5c3aee6131590612e
Allen Lin <linallen5460@gmail.com>
2016-07-04 10:58:00 +1000

    Jython

---
+++ PythonCode/PyCode/Basic.py
@@ -0,0 +1,22 @@
+'''
+Created on 4 Jul 2016
+
+@author: Allen
+'''
+
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 5
+    return y + a
+  
+print(add(1,2))
+print(addFixedValue(1)) 
+
+i = 1
+for i in range(1, 10):
+    if i <= 5 :
+        print(i, 'is smaller or equal then 5.\n'),
+    else:
+        print(i, 'is larger then 5.\n'), 
